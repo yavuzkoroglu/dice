213727084290c2062a957a9c92bf2feb236485aa
yavuzkoroglu <yavuz.koroglu@boun.edu.tr>
2024-11-24 14:59:51 +0300

    [2024-11-24 14:59:51]

--- README.md
+++ README.md
@@ -3,12 +3,20 @@ Generates 'm' lines of 'n' random integers between 'a'-'b', inclusive, using the
 
 If no parameters given, it generates one line of two random integers between 1-6, inclusive.
 
+## How to Build
+
+You may use the `clang` compiler to build the `dice.out` executable:
+
+```
+clang -Weverything -Wno-unsafe-buffer-usage dice.c -o dice.out
+```
+
 ## Examples
 
 ```
-cc dice.c -o dice.exe; ./dice.exe -a 0 -b 1 -m 1 -n 20 -s 12345
+./dice.out -a 0 -b 1 -m 1 -n 20 -s 12345
 ```
 
 ```
-cc dice.c -o dice.out; ./dice.out -m 5 -n 6
+./dice.out -m 5 -n 6
 ```
--- dice.c
+++ dice.c
@@ -1,49 +1,45 @@
 #include <assert.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-#define RAND_DICE(a, b) (rand() % (b - a + 1) + a)
+#define ERROR(fmt)      error("\n ERROR:"fmt"\n"USAGE_FMT, argv[0])
+#define ERROR2(fmt, x)  error("\n ERROR:"fmt"\n"USAGE_FMT, x, argv[0])
+#define RAND_DICE(a, b) ((unsigned)rand() % (b - a + 1) + a)
+#define RAND_SEED       ((unsigned)clock() + (unsigned)time(NULL))
+#define REPEAT(n)       for (unsigned i = (unsigned)(n); i > 0; i--)
 #define USAGE_FMT       "\n Usage: %s [-a|b|m|n|s <positive-integer>]\n\n"
 
+__attribute__((format(printf, 1, 2)))
+static int error(char const* const restrict fmt, ...) {
+    va_list args;
+    assert(fmt != NULL);
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+    return EXIT_FAILURE;
+}
+
 int main(int argc, char* argv[]) {
-    unsigned a = 1;
-    unsigned b = 6;
-    unsigned m = 1;
-    unsigned n = 2;
-    unsigned s = 0;
+    unsigned
+        a = 1, b = 6,
+        m = 1, n = 2,
+        s = 0;
 
     for (int arg_id = 1; arg_id < argc; arg_id += 2) {
-        int const arg_a = !strcmp(argv[arg_id], "-a");
-        int const arg_b = !strcmp(argv[arg_id], "-b");
-        int const arg_m = !strcmp(argv[arg_id], "-m");
-        int const arg_n = !strcmp(argv[arg_id], "-n");
-        int const arg_s = !strcmp(argv[arg_id], "-s");
-        int const sum   = arg_a + arg_b + arg_m + arg_n + arg_s;
+        int const
+            arg_a = !strcmp(argv[arg_id], "-a"),
+            arg_b = !strcmp(argv[arg_id], "-b"),
+            arg_m = !strcmp(argv[arg_id], "-m"),
+            arg_n = !strcmp(argv[arg_id], "-n"),
+            arg_s = !strcmp(argv[arg_id], "-s"),
+            sum   = arg_a + arg_b + arg_m + arg_n + arg_s;
         assert(sum == 0 || sum == 1);
 
-        if (sum == 0) {
-            fprintf(
-                stderr,
-                "\n ERROR: Unrecognized argument '%s'\n"
-                USAGE_FMT,
-                argv[arg_id],
-                argv[0]
-            );
-            return EXIT_FAILURE;
-        }
-
-        if (arg_id + 1 >= argc) {
-            fprintf(
-                stderr,
-                "\n ERROR: Missing value after '%s'\n"
-                USAGE_FMT,
-                argv[arg_id],
-                argv[0]
-            );
-            return EXIT_FAILURE;
-        }
+        if (sum == 0)           ERROR2("Unrecognized argument '%s'", argv[arg_id]);
+        if (arg_id + 1 >= argc) ERROR2("Missing value after '%s'", argv[arg_id]);
 
         if (
             (arg_a && sscanf(argv[arg_id + 1], "%u", &a) != 1) ||
@@ -51,40 +47,19 @@ int main(int argc, char* argv[]) {
             (arg_m && sscanf(argv[arg_id + 1], "%u", &m) != 1) ||
             (arg_n && sscanf(argv[arg_id + 1], "%u", &n) != 1) ||
             (arg_s && sscanf(argv[arg_id + 1], "%u", &s) != 1)
-        ) {
-            fprintf(
-                stderr,
-                "\n ERROR: The value after '%s' must be a positive integer\n"
-                USAGE_FMT,
-                argv[arg_id],
-                argv[0]
-            );
-            return EXIT_FAILURE;
-        }
+        ) ERROR2("The value after '%s' must be a positive integer", argv[arg_id]);
     }
 
-    if (s == 0) {
-        s = (unsigned)clock() + (unsigned)time(NULL);
-    }
+    if (s == 0) s = RAND_SEED;
     srand(s);
 
-    if (a > b) {
-        fputs("\n ERROR: 'a' cannot be larger than 'b'\n\n", stderr);
-        return EXIT_FAILURE;
-    }
+    if (a > b)          ERROR("'a' cannot be larger than 'b'");
+    if (b > RAND_MAX)   ERROR2("'b' cannot be larger than %d", RAND_MAX);
 
-    if (b > RAND_MAX) {
-        fprintf(stderr, "\n ERROR: 'b' cannot be larger than %u\n\n", RAND_MAX);
-        return EXIT_FAILURE;
-    }
-
-    if (n == 0) {
-        return EXIT_SUCCESS;
-    }
+    if (n == 0) return EXIT_SUCCESS;
 
     while (m--) {
-        unsigned i = n;
-        while (--i) printf("%u ", RAND_DICE(a, b));
+        REPEAT(n-1) printf("%u ", RAND_DICE(a, b));
         printf("%u\n", RAND_DICE(a, b));
     }
 

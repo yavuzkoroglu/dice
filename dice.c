a23c0c054584a0a3473fa72c7a740557d6589cec
yavuzkoroglu <yavuz.koroglu@boun.edu.tr>
2024-11-24 15:17:14 +0300

    [2024-11-24 15:17:14]

--- dice.c
+++ dice.c
@@ -5,11 +5,13 @@
 #include <string.h>
 #include <time.h>
 
+#define DEF_ARG(x)      arg_##x = !strcmp(argv[arg_id], "-"#x)
 #define ERROR(fmt)      error("\n ERROR:"fmt"\n"USAGE_FMT, argv[0])
 #define ERROR2(fmt, x)  error("\n ERROR:"fmt"\n"USAGE_FMT, x, argv[0])
 #define RAND_DICE(a, b) ((unsigned)rand() % (b - a + 1) + a)
 #define RAND_SEED       ((unsigned)clock() + (unsigned)time(NULL))
 #define REPEAT(n)       for (unsigned i = (unsigned)(n); i > 0; i--)
+#define SCAN_ARG(x)     (arg_##x && sscanf(argv[arg_id + 1], "%u", &x) != 1)
 #define USAGE_FMT       "\n Usage: %s [-a|b|m|n|s <positive-integer>]\n\n"
 
 __attribute__((format(printf, 1, 2)))
@@ -23,41 +25,26 @@ static int error(char const* const restrict fmt, ...) {
 }
 
 int main(int argc, char* argv[]) {
-    unsigned
-        a = 1, b = 6,
-        m = 1, n = 2,
-        s = 0;
+    unsigned a = 1, b = 6, m = 1, n = 2, s = RAND_SEED;
 
     for (int arg_id = 1; arg_id < argc; arg_id += 2) {
         int const
-            arg_a = !strcmp(argv[arg_id], "-a"),
-            arg_b = !strcmp(argv[arg_id], "-b"),
-            arg_m = !strcmp(argv[arg_id], "-m"),
-            arg_n = !strcmp(argv[arg_id], "-n"),
-            arg_s = !strcmp(argv[arg_id], "-s"),
-            sum   = arg_a + arg_b + arg_m + arg_n + arg_s;
-        assert(sum == 0 || sum == 1);
+            DEF_ARG(a), DEF_ARG(b), DEF_ARG(m), DEF_ARG(n), DEF_ARG(s),
+            sum = arg_a + arg_b + arg_m + arg_n + arg_s;
 
-        if (sum == 0)           ERROR2("Unrecognized argument '%s'", argv[arg_id]);
-        if (arg_id + 1 >= argc) ERROR2("Missing value after '%s'", argv[arg_id]);
+        if (sum == 0)           return ERROR2("Unrecognized argument '%s'", argv[arg_id]);
+        if (arg_id + 1 >= argc) return ERROR2("Missing value after '%s'", argv[arg_id]);
 
-        if (
-            (arg_a && sscanf(argv[arg_id + 1], "%u", &a) != 1) ||
-            (arg_b && sscanf(argv[arg_id + 1], "%u", &b) != 1) ||
-            (arg_m && sscanf(argv[arg_id + 1], "%u", &m) != 1) ||
-            (arg_n && sscanf(argv[arg_id + 1], "%u", &n) != 1) ||
-            (arg_s && sscanf(argv[arg_id + 1], "%u", &s) != 1)
-        ) ERROR2("The value after '%s' must be a positive integer", argv[arg_id]);
+        assert(sum == 1);
+        if (SCAN_ARG(a) || SCAN_ARG(b) || SCAN_ARG(m) || SCAN_ARG(n) || SCAN_ARG(s))
+            return ERROR2("The value after '%s' must be a positive integer", argv[arg_id]);
     }
 
-    if (s == 0) s = RAND_SEED;
+    if (a > b)          return ERROR("'a' cannot be larger than 'b'");
+    if (b > RAND_MAX)   return ERROR2("'b' cannot be larger than %d", RAND_MAX);
+    if (n == 0)         return EXIT_SUCCESS;
+
     srand(s);
-
-    if (a > b)          ERROR("'a' cannot be larger than 'b'");
-    if (b > RAND_MAX)   ERROR2("'b' cannot be larger than %d", RAND_MAX);
-
-    if (n == 0) return EXIT_SUCCESS;
-
     while (m--) {
         REPEAT(n-1) printf("%u ", RAND_DICE(a, b));
         printf("%u\n", RAND_DICE(a, b));
